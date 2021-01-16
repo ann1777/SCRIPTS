6bd4008604a8b7b353dca0b33c7706edc8f80658
Anzhela Potapchuk <75266386+ann1777@users.noreply.github.com>
2021-01-16 22:48:20 +0200

    Bash scripts

---
+++ myscript.sh
@@ -0,0 +1,30 @@
+File Information
+----------------
+Name: myscript (8).sh
+File type: Bash Shell Script (.sh)
+MIME Type: application/x-shellscript
+Opens with: File Viewer Plus 4
+Size: 561 bytes (561 bytes)
+Location: C:\Users\User\Downloads\
+Created: January 16, 2021 10:44 PM
+Modified: January 16, 2021 10:44 PM
+Accessed: January 16, 2021 10:44 PM
+Read-only: No
+Hidden: No
+System: No
+MD5: c275f2674b0e1bed8fd558954f98e09c
+
+File Metadata
+----------------
+MIME Encoding: us-ascii
+Newlines: Unix LF
+Line Count: 25
+Word Count: 82
+
+FileInfo.com Information
+.SH - 1 file type found
+----------------
+Type: Bash Shell Script
+Category: Developer
+Popularity: ★★★★★
+Developer: N/a
